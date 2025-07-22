41fddd0205144d417e9f2c826d5e04a89b0e4f59
Svetlana <74713311+gagarka51@users.noreply.github.com>
2025-07-22 18:51:36 +0300

    Create description.php

---
+++ description.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    'NAME' => 'Календарь',
+    'DESCRIPTION' => 'Выводит календарь с установленными событиями',
+    'CACHE_PATH' => 'Y',
+    'COMPLEX' => 'N',
+    'PATH' => [
+        'ID' => 'ssharkova',
+        'NAME' => 'Шаркова С.',
+    ],
+];
